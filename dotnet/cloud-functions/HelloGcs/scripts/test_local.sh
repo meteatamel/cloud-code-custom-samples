e528ea10e5f25933ca24088985cec304b9f1fc8a
Mete Atamel <atamel@google.com>
2022-11-23 17:07:18 +0000

    Fixed CloudEvents

--- dotnet/cloud-functions/HelloGcs/scripts/test_local.sh
+++ dotnet/cloud-functions/HelloGcs/scripts/test_local.sh
@@ -2,7 +2,25 @@
 
 echo "Trigger the function by sending a Cloud Storage CloudEvent"
 set -v
-curl -X POST \
-    -H "content-type: application/json"  \
-    -d @data.json \
-  http://localhost:8080 -v
+curl localhost:8080 -v \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -H "ce-id: 123451234512345" \
+  -H "ce-specversion: 1.0" \
+  -H "ce-time: 2020-01-02T12:34:56.789Z" \
+  -H "ce-type: google.cloud.storage.object.v1.finalized" \
+  -H "ce-source: //storage.googleapis.com/projects/_/buckets/MY-BUCKET-NAME" \
+  -H "ce-subject: objects/MY_FILE.txt" \
+  -d '{
+        "bucket": "MY_BUCKET",
+        "contentType": "text/plain",
+        "kind": "storage#object",
+        "md5Hash": "...",
+        "metageneration": "1",
+        "name": "MY_FILE.txt",
+        "size": "352",
+        "storageClass": "MULTI_REGIONAL",
+        "timeCreated": "2020-04-23T07:38:57.230Z",
+        "timeStorageClassUpdated": "2020-04-23T07:38:57.230Z",
+        "updated": "2020-04-23T07:38:57.230Z"
+      }'
--- dotnet/cloud-functions/HelloPubSub/scripts/test_local.sh
+++ dotnet/cloud-functions/HelloPubSub/scripts/test_local.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# TODO: Make sure data.json is an actual Pub/Sub CloudEvent.
-
 echo "Trigger the function by sending a Pub/Sub CloudEvent"
 set -v
-curl -X POST \
-    -H "content-type: application/json"  \
-    -d @data.json \
-  http://localhost:8080 -v
+curl localhost:8080 -v \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -H "ce-id: 6308619096677818" \
+  -H "ce-specversion: 1.0" \
+  -H "ce-time: 2020-01-02T12:34:56.789Z" \
+  -H "ce-type: google.cloud.pubsub.topic.v1.messagePublished" \
+  -H "ce-source: //pubsub.googleapis.com/projects/MY-PROJECT/topics/MY-TOPIC" \
+  -d '{
+        "message": {
+          "data": "SGVsbG8gV29ybGQ=",
+          "attributes": {
+             "attr1":"attr1-value"
+          }
+        },
+        "subscription": "projects/MY-PROJECT/subscriptions/MY-SUB"
+      }'
