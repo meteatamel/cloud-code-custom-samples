f6e08ad5a80801c4b2230579a28097d11d127586
marcacohen <marcacohen@gmail.com>
2023-02-27 12:15:14 +0000

    initial check in of python hello-http app

---
+++ python/functions/hello-http/README.md
@@ -0,0 +1,58 @@
+# Python: Cloud Functions - HelloHttp
+
+ An HTTP triggered Python Cloud Functions template.
+
+## Test locally
+
+Install dependencies:
+
+```sh
+pip install -r requirements.txt
+```
+
+Run the function:
+
+```sh
+python3 main.py
+
+...
+Serving function...
+Function: helloHttp
+Signature type: http
+URL: http://localhost:8080/
+```
+
+Inside the [scripts](scripts) folder, run [test_local.sh](scripts/test.sh) to
+test the function locally:
+
+```sh
+./test_local.sh
+
+Hello World!
+```
+
+## Before deploying to Google Cloud
+
+Run [setup.sh](scripts/setup.sh) to enable required services:
+
+```sh
+./setup.sh
+```
+
+## Deploy to Google Cloud
+
+Run [deploy.sh](scripts/deploy.sh) to deploy to Google Cloud:
+
+```sh
+./deploy.sh
+```
+
+## Test in Google Cloud
+
+You can test with `Invoke function via HTTP` in Cloud Code or with `gcloud`.
+
+Run [test_cloud.sh](scripts/test_cloud.sh) to test the function in Google Cloud:
+
+```sh
+./test_cloud.sh
+```
---
+++ python/functions/hello-http/main.py
@@ -0,0 +1,29 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+
+@functions_framework.http
+def hello_get(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    Note:
+        For more information on how Flask integrates with Cloud
+        Functions, see the `Writing HTTP functions` page.
+        <https://cloud.google.com/functions/docs/writing/http#http_frameworks>
+    """
+    return 'Hello World!'
