628420e2e42f2af6c7d81f67bcba8275667a4c06
marcacohen <marcacohen@gmail.com>
2023-03-05 13:45:11 +0000

    Revert "propagate auditlog fix from functions to run"
    This reverts commit e637135616017717af1686d5263d3e13398ed3da.

--- python/run/hello-auditlog/main.py
+++ python/run/hello-auditlog/main.py
@@ -14,20 +14,25 @@
 import json
 from flask import Flask, request
 from cloudevents.http import from_http
+# FIXME: This ends up with ModuleNotFoundError: No module named 'google.api'
+from google.events.cloud.audit_v1 import LogEntryData
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def hello_auditlog():
     cloud_event = from_http(request.headers, request.get_data())
+
     print(f"Event ID: {cloud_event['id']}")
     print(f"Event Type: {cloud_event['type']}")
 
-    payload = cloud_event.data.get("protoPayload")
+    log_entry_data = LogEntryData.from_json(json.dumps(cloud_event.data))
+    payload = log_entry_data.proto_payload
     if payload:
-        print(f"ServiceName: {payload.get('service_name')}")
-        print(f"MethodName: {payload.get('method_name')}")
-        print(f"ResourceName: {payload.get('resource_name')}")
+      print(f"ServiceName: {payload.service_name}")
+      print(f"MethodName: {payload.method_name}")
+      print(f"ResourceName: {payload.resource_name}")
+
     return "", 204
 
 if __name__ == '__main__':
